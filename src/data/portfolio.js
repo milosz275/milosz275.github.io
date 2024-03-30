73b3da581ba5c95607e69ebad5c31fcbc35b88b6
mldxo <miloszmaculewicz@gmail.com>
2024-03-30 14:42:23 +0100

    Update assets url

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -3,70 +3,70 @@ export default [
         title: "UAV Collision Avoidance",
         description: "PyQt6 application that simulates a UAV flying in a 3D environment. The UAVs are able to avoid collision using a simple collision avoidance algorithm.",
         stack: ["Python", "PyQt6"],
-        imgUrl: "",
+        imgUrl: "/assets/uav.png",
         link: "https://github.com/mldxo/uav-collision-avoidance"
     },
     {
         title: "Qt Online Messenger",
         description: "Simple online messenger application built with C++ and Qt. The application allows users to send messages to each other in real-time using Windows sockets.",
         stack: ["C++", "Qt", "Winsock"],
-        imgUrl: "https://raw.githubusercontent.com/mldxo/mldxo/main/src/messenger.png",
+        imgUrl: "/assets/messenger.png",
         link: "https://github.com/mldxo/qt-messenger"
     },
     {
         title: "Checkers game",
         description: "Checkers game built with C++ and SFML. The game allows players to play against each other or against a bot. The bot uses the minimax algorithm with alpha-beta pruning to make decisions.",
         stack: ["C++", "SFML", "Ai"],
-        imgUrl: "https://raw.githubusercontent.com/mldxo/mldxo/main/src/checkers.png",
+        imgUrl: "/assets/checkers.png",
         link: "https://github.com/mldxo/checkers-gui"
     },
     {
         title: "Battleship console game",
         description: "Battleship console game built with C++. The game allows players to play against each other or against a bot. The bot uses a simple algorithm to make decisions.",
         stack: ["C++", "Ai"],
-        imgUrl: "https://raw.githubusercontent.com/mldxo/mldxo/main/src/battleship.png",
+        imgUrl: "/assets/battleship.png",
         link: "https://github.com/mldxo/battleship-console-game"
     },
     {
         title: "RSA Generation/Encryption",
         description: "Simple RSA key generation and encryption/decryption application built with C++. The application allows users to generate RSA keys and encrypt/decrypt messages.",
         stack: ["C++", "libgmp"],
-        imgUrl: "https://raw.githubusercontent.com/mldxo/mldxo/main/src/rsa.png",
+        imgUrl: "/assets/rsa.png",
         link: "https://github.com/mldxo/rsa-keygen"
     },
     {
         title: "Classification Methods Comparison",
         description: "R/Jupyter Notebook implementation of a comparison of classification methods. The comparison includes the k-NN, SVM, and Random Forest algorithms. The comparison is based on the accuracy of the algorithms on a few dataset",
         stack: ["R", "Jupyter Notebook"],
-        imgUrl: "",
+        imgUrl: "/assets/classification.png",
         link: "https://github.com/mldxo/classification-methods-comparison"
     },
     {
         title: "Dining Philosophers Problem",
         description: "Python implementation of simulation of the dining philosophers problem. The simulation optimizes the parameters of the problem to showcase the most efficient solution.",
         stack: ["Python", "sync"],
-        imgUrl: "",
+        imgUrl: "/assets/philosophers.png",
         link: "https://github.com/mldxo/dining-philosophers"
     },
     {
         title: "Python Package Template",
         description: "Python package template that can be used to create new Python packages. The template includes a setup.py file, a README.md file, and a LICENSE file. The template is ready to be published on PyPI.",
         stack: ["Python", "PyPI"],
-        imgUrl: "",
+        imgUrl: "/assets/package.png",
         link: "https://github.com/mldxo/python-package-template"
     },
     {
         title: "Social Networking Platform",
         description: "Social networking platform built with React and Django. The platform allows users to create posts, add likes, comments and follow other users. The platform will also include a chat feature.",
         stack: ["HTML", "CSS", "JavaScript", "React", "Python", "Django"],
-        imgUrl: "",
+        imgUrl: "/assets/social.png",
         link: "https://github.com/himokkk/social-networking-platform"
     },
     {
         title: "mldxo.github.io",
         description: "This minimal portfolio was built with React and Tailwind CSS. It is a single page application that showcases my work and experience.",
         stack: ["HTML", "CSS", "JavaScript", "React", "Tailwind CSS"],
-        imgUrl: "",
+        imgUrl: "/assets/portfolio.png",
         link: "https://github.com/mldxo/mldxo.github.io"
     },
 ]
