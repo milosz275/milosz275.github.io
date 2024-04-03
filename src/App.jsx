b6668946594ba3900902fb43a059204ccb32b86a
mldxo <miloszmaculewicz@gmail.com>
2024-04-03 23:48:00 +0200

    Transition to hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import MainPage from "./pages/MainPage"
 import ErrorPage from "./pages/ErrorPage"
 import * as urls from './urls';
@@ -14,7 +14,7 @@ class App extends React.Component {
         return (React.createElement(
 		'div',
 		{ className: 'App' },
-		React.createElement(BrowserRouter, null,
+		React.createElement(HashRouter, null,
 		React.createElement(Routes, null,
 		React.createElement(Route, { path: urls.ROOT_URL, element: React.createElement(MainPage, {}) }),
 		React.createElement(Route, { path: '*', element: React.createElement(ErrorPage, {}), })
