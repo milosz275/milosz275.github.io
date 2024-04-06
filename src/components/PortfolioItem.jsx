c67115c42ce14a10b53179728f00a44179fd8ae7
mldxo <miloszmaculewicz@gmail.com>
2024-04-06 12:09:13 +0200

    Fix multiple handleClick calls

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -18,8 +18,7 @@ function PortfolioItem({title, description, imgUrl, stack, link}) {
       className="border-2 border-stone-900 dark:border-white rounded-md overflow-hidden md:hover:bg-slate-200 \
       md:dark:hover:bg-github lg:hover:bg-slate-200 lg:dark:hover:bg-github cursor-pointer md:transition md:duration-300 \
       md:ease-in-out md:hover:scale-105 md:dark:hover:scale-105 lg:transition lg:duration-300 \
-      lg:ease-in-out lg:hover:scale-105 lg:dark:hover:scale-105"
-      onClick={handleClick(link)}>
+      lg:ease-in-out lg:hover:scale-105 lg:dark:hover:scale-105">
         <div className="w-full p-4 mt-3 cursor-pointer">
           <div className="flex flex-col w-full h-40" onClick={handleClick(link)}>
             <h3 className="h-full text-lg md:text-xl dark:text-white mb-2 md:mb-3 font-semibold select-none">
