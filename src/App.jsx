203331f62c0050a48dbaedd32e1eeae743d03e61
mldxo <miloszmaculewicz@gmail.com>
2024-06-24 15:01:53 +0200

    Remove subpages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage"
-import UAVPage from "./pages/UAVPage"
-import PicoLedPage from "./pages/PicoLedPage"
 import ErrorPage from "./pages/ErrorPage"
 import * as urls from "./urls";
 
@@ -28,8 +26,6 @@ class App extends React.Component {
 		React.createElement(BrowserRouter, null,
 		React.createElement(Routes, null,
 		React.createElement(Route, { path: urls.ROOT_URL, element: React.createElement(MainPage, {}) }),
-		React.createElement(Route, { path: urls.UAV_URL, element: React.createElement(UAVPage, {}) }),
-		React.createElement(Route, { path: urls.PICO_LED_URL, element: React.createElement(PicoLedPage, {}) }),
 		React.createElement(Route, { path: "*", element: React.createElement(ErrorPage, {}), })
 		))));
 	}
--- src/urls.js
+++ src/urls.js
@@ -1,3 +1,1 @@
 export const ROOT_URL = "/";
-export const UAV_URL = "/uav-collision-avoidance";
-export const PICO_LED_URL = "/pico-led-controller";
