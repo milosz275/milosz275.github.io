a354093ee6ff28c4d758374ef1640a46640d1bea
Miłosz Maculewicz <miloszmaculewicz@gmail.com>
2024-07-08 22:09:43 +0200

    Update timeline.js

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -15,14 +15,14 @@ export default [
     },
     {
         year: "2023",
-        title: "Software Engineer",
+        title: "Summer Internship",
         duration: "3 months",
         details:
             "I took part in designing data retrieval and analysis processes using scripting. I also contributed to the creation of robust network architecture, ensuring optimal performance and security. My role required strong collaboration with the team and a deep understanding of software engineering principles.",
     },
     {
         year: "Today",
-        title: "Tech savvy passionate",
+        title: "Digital Nomad",
         duration: "7 years",
         details:
             "My journey into the world of technology and software development began with a simple curiosity that blossomed into a passionate pursuit. It all started with Adobe's Master Collection programs, where I dabbled in graphic design and video editing, creating banners, logos, and videos for various projects. \n As my skills grew, so did my thirst for knowledge. I ventured into web development, studying HTML, CSS, and JavaScript, and then took the leap into learning C++ on my own. Eventually, I enrolled in a Computer Science course to deepen my understanding. \n Each step of this journey has been a testament to my unwavering dedication and thirst for knowledge, as I continue to explore the ever-evolving landscape of technology and programming.",
