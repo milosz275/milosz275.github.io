ca3b9dccb126821f88aa4a0cad15cbcc89bdaeba
milosz275 <miloszmaculewicz@gmail.com>
2025-03-25 18:55:42 +0100

    Update featured projects

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -32,12 +32,12 @@ function Intro() {
 
   const featuredProjects = [
     {
-      name: "Secure Chat",
-      link: "https://github.com/milosz275/secure-chat"
+      name: "Graph Visualizer",
+      link: "https://github.com/milosz275/graph-visualizer"
     },
     {
-      name: "Blog App",
-      link: "https://github.com/milosz275/blog-app"
+      name: "Bitlab",
+      link: "https://github.com/milosz275/bitlab"
     },
   ];
 
@@ -50,14 +50,21 @@ function Intro() {
         Hi, I'm Miłosz, a software engineer based in Poland.
       </p>
       <p className="text-sm dark:text-white max-w-xl mb-6 font-normal">
-        I'm a fourth-year Computer Science student, constantly striving to develop my skills and forge my own path in the tech world. Currently, I'm working on a <a href={featuredProjects[0].link} target="_blank" rel="noreferrer noopener" className="text-cyan-600 hover:underline underline-offset-2 decoration-1 decoration-purple-400">{featuredProjects[0].name}</a> project, always seeking new challenges and opportunities to grow.
+        I'm a fourth-year Computer Science student, constantly striving to develop my skills and forge my own path in the tech world. Currently, I'm working on a <a
+          href={featuredProjects[0].link}
+          target="_blank"
+          rel="noreferrer noopener"
+          className="text-cyan-600 underline-offset-2 decoration-1 decoration-purple-400 hover-underline-animation"
+        >
+          {featuredProjects[0].name}
+        </a> project, always seeking new challenges and opportunities to grow.
       </p>
       <div className={`opacity-0 ${hasScrolled ? "opacity-100 translate-y-0" : "translate-y-10"} transition-opacity duration-700 ease-out`}>
-        {/* <div className="flex flex-row space-x-2">
+        <div className="flex flex-row space-x-2">
           <a href="https://wakatime.com/@54a5e85a-a806-4d39-bc8b-38523f717b15" target="_blank" rel="noreferrer noopener">
             <img src="https://wakatime.com/badge/user/54a5e85a-a806-4d39-bc8b-38523f717b15.svg" alt="Total time coded since May 27 2024" className="hue-rotate" />
           </a>
-        </div> */}
+        </div>
       </div>
     </div>
   );
