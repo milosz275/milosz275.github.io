6ca9e97b37fb8774cea87bb670b0b4817370b032
milosz275 <miloszmaculewicz@gmail.com>
2025-03-25 18:55:09 +0100

    Add graph visualizer

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -11,13 +11,13 @@ export default [
     },
     {
         id: 2,
-        title: "Secure Chat",
-        description: "Text communication application built with C. The chat allows users to exchange messages in real-time using a secure connection based on HTTPS and end-to-end encryption. SQLite3 is used as a database to store user data and encrypted messages.",
-        timeInterval: ["June 2024", "October 2024"],
-        docs: ["Docs", "https://milosz275.github.io/secure-chat"],
-        stack: ["C", "Make", "CMake", "OpenSSL", "SQLite3", "Raylib", "Doxygen"],
-        imgUrl: "/assets/secure-chat.png",
-        link: "https://github.com/milosz275/secure-chat"
+        title: "Graph Visualizer",
+        description: "WebAssembly project implementing force-directed graphs and algorithms in real-time. Program based on MVC, state and other design patterns, following the principles of software engineering. GitHub Pages used for automatic demo deployment.",
+        timeInterval: ["February 2025", "March 2025"],
+        docs: ["Demo", "https://milosz275.github.io/graph-visualizer/demo"],
+        stack: ["C++", "Make", "Emscripten", "Design Patterns", "Graph Algorithms", "JavaScript", "Doxygen"],
+        imgUrl: "/assets/graph-visualizer.png",
+        link: "https://github.com/milosz275/graph-visualizer"
     },
     {
         id: 3,
@@ -31,16 +31,6 @@ export default [
     },
     {
         id: 4,
-        title: "Blog App",
-        description: "Full-stack blog application built with Python3, Django, HTML, CSS, JavaScript, React, Vite and Tailwind CSS. The application allows users to create, edit and delete posts. The application is part of my portfolio.",
-        timeInterval: ["August 2024", "now"],
-        docs: ["Site", "https://milosz275.github.io/blog-app"],
-        stack: ["Python3", "Django", "HTML", "CSS", "JavaScript", "React", "Vite", "Tailwind CSS"],
-        imgUrl: "/assets/blog.png",
-        link: "https://github.com/milosz275/blog-app"
-    },
-    {
-        id: 5,
         title: "Leetcode Solutions",
         description: "Collection of solutions to Leetcode problems. C, C++ and Python are used. Repo present my approach to solving the problems and the urge to improve my coding skills.",
         timeInterval: ["July 2024", "now"],
@@ -50,7 +40,7 @@ export default [
         link: "https://github.com/milosz275/leetcode"
     },
     {
-        id: 6,
+        id: 5,
         title: "Checkers Game",
         description: "Checkers game built with C++ and SFML. The game allows players to play against each other or against a bot using the minimax algorithm with alpha-beta pruning to make decisions.",
         timeInterval: ["March 2023", "July 2023"],
@@ -60,7 +50,7 @@ export default [
         link: "https://github.com/milosz275/checkers-gui"
     },
     {
-        id: 7,
+        id: 6,
         title: "Qt Online Messenger",
         description: "Application built with C++ and Qt6. The application allows users to send and receive messages between network users in real-time using Windows sockets.",
         timeInterval: ["February 2023", "April 2023"],
@@ -70,7 +60,7 @@ export default [
         link: "https://github.com/milosz275/qt-messenger"
     },
     {
-        id: 8,
+        id: 7,
         title: "Battleship CLI Game",
         description: "Battleship console game built with C++ allowing players to play against each other or against a bot. The bot uses a simple algorithm based on game's state to make decisions.",
         timeInterval: ["February 2023", "April 2023"],
@@ -80,7 +70,7 @@ export default [
         link: "https://github.com/milosz275/battleship-console-game"
     },
     {
-        id: 9,
+        id: 8,
         title: "RSA Generation/Encryption",
         description: "Simple RSA key generation and encryption/decryption application built with C++. The application allows users to generate and load RSA keys and then encrypt/decrypt messages.",
         timeInterval: ["March 2023", "May 2023"],
@@ -90,7 +80,7 @@ export default [
         link: "https://github.com/milosz275/rsa-keygen"
     },
     {
-        id: 10,
+        id: 9,
         title: "GitHub Follow Manager",
         description: "Bash script that allows users to manage their GitHub followers. Includes features like following all followers, unfollowing all followers, following all users that follow a specific user and more using the GitHub API.",
         timeInterval: ["August 2024", "September 2024"],
@@ -100,7 +90,7 @@ export default [
         link: "https://github.com/milosz275/github-follow-manager"
     },
     {
-        id: 11,
+        id: 10,
         title: "Classification Methods Comparison",
         description: "R/Jupyter Notebook comparison of the following classification method: k-NN, SVM, and Random Forest algorithms. The comparison is based on the accuracy of the algorithms on following datasets: Iris, Penguins and Hawks",
         timeInterval: ["December 2023", "January 2024"],
@@ -110,17 +100,7 @@ export default [
         link: "https://github.com/milosz275/classification-methods-comparison"
     },
     {
-        id: 12,
-        title: "Dining Philosophers Problem",
-        description: "Python implementation of simulation of the dining philosophers problem. The simulation optimizes the parameters of the problem to showcase the most efficient solution.",
-        timeInterval: ["April 2024", "May 2024"],
-        docs: [],
-        stack: ["Python3", "matplotlib", "PyPI"],
-        imgUrl: "/assets/philosophers.png",
-        link: "https://github.com/milosz275/dining-philosophers"
-    },
-    {
-        id: 13,
+        id: 11,
         title: "Python Package Template",
         description: "Python3 package template that can be used to create new PyPI packages. The template includes a pyproject.toml file, an expandable README.md, a MIT license and sample tests. The template is ready to be published on PyPI.",
         timeInterval: ["February 2024"],
@@ -130,7 +110,7 @@ export default [
         link: "https://github.com/milosz275/python-package-template"
     },
     {
-        id: 14,
+        id: 12,
         title: "C/C++ Template",
         description: "C/C++ template that can be used to create new projects. The template includes a Makefile, a README.md, a MIT license and sample tests. The template is ready to be used in new projects including Doxygen deployment to gh-pages.",
         timeInterval: ["June 2024"],
@@ -140,7 +120,7 @@ export default [
         link: "https://github.com/milosz275/c-cpp-template"
     },
     {
-        id: 15,
+        id: 13,
         title: "Pico W Template",
         description: "C/C++ template that can be used to create new RPi Pico W projects. The template utilizes CMake and contains sample code for WiFi connection and multi-core operations. The template is ready to be used in new projects including Doxygen deployment to gh-pages.",
         timeInterval: ["October 2024"],
@@ -150,7 +130,7 @@ export default [
         link: "https://github.com/milosz275/pico-w-template"
     },
     {
-        id: 16,
+        id: 14,
         title: "Social Networking Platform",
         description: "Social networking platform built with React and Django. The platform allows users to create posts, add likes, comments and follow other users. The platform will also include a chat feature.",
         timeInterval: ["November 2023", "January 2024"],
@@ -160,7 +140,7 @@ export default [
         link: "https://github.com/himokkk/social-networking-platform"
     },
     {
-        id: 17,
+        id: 15,
         title: "Portfolio",
         description: "This repository is using GitHub Pages to deploy a Portfolio, that includes a list of projects, a short bio, and a contact form. The website is built with React, Vite, and Tailwind CSS. Subpages are generated dynamically based on the projects data.",
         timeInterval: ["April 2024", "May 2024"],
